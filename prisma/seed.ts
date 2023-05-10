dc7c0a074cc4bde24ff2daecf42866513cd995df
Eduardo Souza <edcampossouza@gmail.com>
2023-05-09 21:55:56 -0300

    feat: add TicketType seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,16 +7,26 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
 
   console.log({ event });
+
+  const nTickets = await prisma.ticketType.count();
+  if (nTickets < 3)
+    await prisma.ticketType.createMany({
+      data: [
+        { name: 'Online', isRemote: true, includesHotel: false, price: 100 },
+        { name: 'Sem Hotel', isRemote: false, includesHotel: false, price: 250 },
+        { name: 'Com hotel', isRemote: false, includesHotel: true, price: 600 },
+      ],
+    });
 }
 
 main()
